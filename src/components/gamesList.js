4c0954010be4e6ddac0da14558f387b930203a37
Andrew Haight <haight.haight@gmail.com>
2021-11-01 16:58:02 -0700

    games list / card generation build good

---
+++ src/components/gamesList.js
@@ -0,0 +1,54 @@
+// TODO IMAGES not showing up unless externial link 
+
+const games = [
+    {
+        title: "Rock Paper Scissors",
+        img: "https://placekitten.com",
+        tech: "Tech: HTML/CSS/JavaScript, ReactJs",
+        description: "Basic good 'ol game of Rock Paper Scissors.",
+        play: "RockPaperScissor",
+        id:"1",
+    },
+    {
+        title: "Tic Tac Toe",
+        img: "https://placekitten.com",
+        tech: "Tech: HTML/CSS/JavaScript, ReactJs",
+        description: "Tic, Tac, Toe, or Naughts and Crosses, Xs and Os... you get it.",
+        play: "./ticTacToe",
+        id:"2",
+    },
+    {
+        title: "Connect Four",
+        img: "Placeholder",
+        tech: "PlaceHolder",
+        description: "PlaceHolder PlaceHolder PlaceHolder PlaceHolder PlaceHolder PlaceHolder",
+        play: "./rockPaperScissor",
+        id:"3",
+    },
+    {
+        title: "Placeholder",
+        img: "Placeholder",
+        tech: "Placeholder",
+        description: "Placeholder",
+        play: "./rockPaperScissor",
+        id:"4",
+    },
+    {
+        title: "Placeholder",
+        img: "Placeholder",
+        tech: "Placeholder",
+        description: "Placeholder.",
+        play: "./rockPaperScissor",
+        id:"5",
+    },
+    {
+        title: "Placeholder",
+        img: "Placeholder",
+        tech: "Placeholder",
+        description: "Placeholder.",
+        play: "./rockPaperScissor",
+        id:"6",
+    },
+]
+
+export default games;
