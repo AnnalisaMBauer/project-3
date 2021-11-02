aedc30e007d512878134074d3a7f70f329774e1f
Tucker Beauchamp <tuckerbeauchamp@gmail.com>
2021-11-02 15:11:42 -0700

    add rps

--- client/src/pages/RockPaperScissor.js
+++ client/src/pages/RockPaperScissor.js
@@ -1,61 +1,77 @@
-import { useEffect, useState } from 'react'
+import { useState } from "react";
 
 const RockPaperScissors = () => {
-  const [userChoice, setUserChoice] = useState(null)
-  const [computerChoice, setComputerChoice] = useState(null)
-  const [result, setResult] = useState(null)
-  const choices = ['rock', 'paper', 'scissors']
+  const choices = ["rock", "paper", "scissors"];
+  const [userChoice, setUserChoice] = useState("");
+  const [computerChoice, setComputerChoice] = useState("");
+  const [result, setResult] = useState("");
+  const [scores, setScores] = useState({
+    wins: 0,
+    losses: 0,
+    ties: 0,
+  });
 
   const handleClick = (value) => {
-    setUserChoice(value)
-    generateComputerChoice()
-  }
+    const computerVal = generateComputerChoice();
+
+    if (
+      (computerVal === "rock" && value === "paper") ||
+      (computerVal === "paper" && value === "scissors") ||
+      (computerVal === "scissors" && value === "rock")
+    ) {
+      setScores({
+        ...scores,
+        wins: scores.wins + 1,
+      });
+      setResult("Won!")
+    } else if (computerVal === value) {
+      setScores({
+        ...scores,
+        ties: scores.ties + 1,
+      });
+      setResult("Tied!")
+    } else {
+      setScores({
+        ...scores,
+        losses: scores.losses + 1,
+      });
+      setResult("Lost!")
+    }
+    setUserChoice(value);
+    setComputerChoice(computerVal);
+  };
 
   const generateComputerChoice = () => {
-    const randomChoice = choices[Math.floor(Math.random() * choices.length)]
-    setComputerChoice(randomChoice)
-  }
-
-  useEffect(() => {
-    {
-      switch (userChoice + computerChoice) {
-        case 'scissorspaper':
-        case 'rockscissors':
-        case 'paperrock':
-          setResult('YOU WIN!')
-          break
-        case 'paperscissors':
-        case 'scissorsrock':
-        case 'rockpaper':
-          setResult('YOU LOSE!')
-          break
-        case 'rockrock':
-        case 'paperpaper':
-        case 'scissorsscissors':
-          setResult('ITS A DRAW!')
-          break
-      }
-    }
-  }, [computerChoice, userChoice])
+    return choices[Math.floor(Math.random() * choices.length)];
+  };
 
   return (
     <div className="d-flex col-12 justify-content-center align-items-center mt-5">
       <div className="p-5">
-        <div className='' >
+        <div>
+          <p>Wins: {scores.wins}</p>
+          <p>Losses: {scores.losses}</p>
+          <p>Ties: {scores.ties}</p>
+        </div>
+        <div className="">
           <h1>user choice is: {userChoice}</h1>
           <h1>computer choice is: {computerChoice}</h1>
         </div>
-        <div className=''>
-          {choices.map((choice, index) =>
-        <button className="m-2" key={index} onClick={() => handleClick(choice)}>
-          {choice}
-        </button>
-      )}
+        <div className="">
+          {choices.map((choice, index) => (
+            <button
+              className="m-2"
+              key={index}
+              onClick={() => handleClick(choice)}
+            >
+              {choice}
+            </button>
+          ))}
+        </div>
+        <h1>{result}</h1>
       </div>
-      <h1>{result}</h1>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default RockPaperScissors
+export default RockPaperScissors;
