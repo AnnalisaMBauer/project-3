dd832c3d04e81c98e838e50d098cd97ec5b5829a
Tucker Beauchamp <tuckerbeauchamp@gmail.com>
2021-11-02 14:59:30 -0700

    add react router

---
+++ client/src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import games from "../components/gamesList";
+import GamesCard from "../components/gamesCard";
+import Heading from "../components/heading";
+
+function Body() {
+  return (
+    <div>
+      <Heading />
+
+      <div className="d-flex justify-content-center flex-wrap p-3 mb-5">
+        {games.map((game) => {
+          return <GamesCard games={game} key={game.id} />;
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Body;
