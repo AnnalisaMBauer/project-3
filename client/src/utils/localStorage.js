0279d7f2e549000a69edb80e0bc5f7038b8f7d4e
young <brevinyoung@gmail.com>
2021-11-03 10:23:47 -0700

    delete before pull

--- client/src/utils/localStorage.js
+++
@@ -1,8 +0,0 @@
-export const getSavedHighscoreIds = () => {
-    const savedHighscoreIds = localStorage.getItem('saved_highscores')
-      ? JSON.parse(localStorage.getItem('saved_highscores'))
-      : [];
-  
-    return savedHighscoreIds ;
-  };
-  
