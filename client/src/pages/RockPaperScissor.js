02af2b41dd77b38284adc2b75efeb5eed26724d0
Andrew Haight <haight.haight@gmail.com>
2021-11-04 09:50:25 -0700

    adjusting animation styles

--- client/src/pages/RockPaperScissor.js
+++ client/src/pages/RockPaperScissor.js
@@ -1,12 +1,13 @@
 import { useState } from "react";
-import Zoom from 'react-reveal/Zoom';
-import Flash from 'react-reveal/Flash';
-import Tada from 'react-reveal/Tada';
+import Zoom from "react-reveal/Zoom";
+import Flash from "react-reveal/Flash";
+import Jello from "react-reveal/Jello";
+import Swing from "react-reveal/Swing";
 import Jumbotron from "./RpsJumbotron";
 
 const RockPaperScissors = () => {
   const [showDiv, setShowDiv] = useState(true);
-  const clickDiv = () => setShowDiv(false)
+  const clickDiv = () => setShowDiv(false);
   const choices = ["🤘", "🧻", "✂️"];
   const [userChoice, setUserChoice] = useState("");
   const [computerChoice, setComputerChoice] = useState("");
@@ -29,19 +30,19 @@ const RockPaperScissors = () => {
         ...scores,
         wins: scores.wins + 1,
       });
-      setResult("You Won!")
+      setResult("You Won!");
     } else if (computerVal === value) {
       setScores({
         ...scores,
         ties: scores.ties + 1,
       });
-      setResult("You Tied!")
+      setResult("You Tied!");
     } else {
       setScores({
         ...scores,
         losses: scores.losses + 1,
       });
-      setResult("You Lost!")
+      setResult("You Lost!");
     }
     setUserChoice(value);
     setComputerChoice(computerVal);
@@ -52,59 +53,64 @@ const RockPaperScissors = () => {
   };
 
   const Style = {
-    fontSize: '5rem',
-    width: '150px',
-    height: '150px',
-    boxShadow: '4px 4px 10px 1px black',
+    fontSize: "5rem",
+    width: "150px",
+    height: "150px",
+    boxShadow: "4px 4px 10px 1px black",
   };
 
   const ContStyle = {
-    display: 'grid',
-    gridTemplateRows: '1fr 1fr',
-    marginTop: '20vh',
-  } 
+    display: "grid",
+    gridTemplateRows: "1fr 1fr",
+    marginTop: "20vh",
+  };
 
   const GameStyle = {
-    gridArea: '1 / 1',
-  }
+    gridArea: "1 / 1",
+  };
 
   // TODO add username of logged in user to h1 'User choice' element
   return (
     <div onClick={clickDiv} class="container mb-5" style={ContStyle}>
-      {showDiv ? <Jumbotron /> : null }
- 
-    <div className="d-flex justify-content-center" style={GameStyle}>
-      
-      <div className="p-1">
+      {showDiv ? <Jumbotron /> : null}
+
+      <div className="d-flex justify-content-center" style={GameStyle}>
+        <div className="p-1">
           <Zoom>
-        <div className="row p-1 justify-content-center">
-          <h1 className="display-5 pt-0">Wins: {scores.wins}</h1>
-          <h1 className="display-5 pt-0 pl-5">Losses: {scores.losses}</h1>
-          <h1 className="display-5 pt-0 pl-5">Ties: {scores.ties}</h1>
-        </div>
-        <div className="text-center">
-          <h1>User's choice is: {userChoice}</h1>
-          <h1>Computer's choice is: {computerChoice}</h1>
-        </div>
-        <div className="row">
-            <Tada spy={handleClick}>
-          {choices.map((choice, index) => (
-            <button style={Style}
-              className=" m-2 btn btn-dark rounded-circle"
-              key={index}
-              onClick={() => handleClick(choice)}
-            >
-              {choice}
-            </button>
-          ))}
-          </Tada>
-        </div>
+            <div className="row p-1 justify-content-center">
+              <h1 className="display-5 pt-0">Wins: {scores.wins}</h1>
+              <h1 className="display-5 pt-0 pl-5">Losses: {scores.losses}</h1>
+              <h1 className="display-5 pt-0 pl-5">Ties: {scores.ties}</h1>
+            </div>
+            <div className="text-center">
+              <h1>
+                User's choice is: <Swing spy={handleClick}>{userChoice}</Swing>
+              </h1>
+              <h1>
+                Computer's choice is:{" "}
+                <Swing spy={handleClick}>{computerChoice}</Swing>
+              </h1>
+            </div>
+            <div className="row">
+              {choices.map((choice, index) => (
+                <Jello spy={handleClick}>
+                  <button
+                    style={Style}
+                    className=" m-2 btn btn-dark rounded-circle"
+                    key={index}
+                    onClick={() => handleClick(choice)}
+                  >
+                    {choice}
+                  </button>
+                </Jello>
+              ))}
+            </div>
           </Zoom>
-        <Flash spy={result}>
-        <h1 className="display-1 text-center mb-5">{result}</h1>
-        </Flash>
+          <Flash spy={result}>
+            <h1 className="display-1 text-center mb-5">{result}</h1>
+          </Flash>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
