59b5776d237c45aae91c648ff51d0df2b8d944f0
Andrew Haight <haight.haight@gmail.com>
2021-11-04 13:43:13 -0700

    home carousel and nav logo added

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -7,7 +7,7 @@ import Auth from "../utils/auth";
 
 const style1 = {
   borderRadius: "25px",
-  margin: "1.5em 0em 2em 0em",
+  margin: "1.5em 0em 1em 0em",
   display: "flex",
   backgroundColor: "rgb(206 216 223 / 50%)",
   alignItems: "center",
@@ -15,11 +15,10 @@ const style1 = {
 };
 const logo = {
   color: "white",
-  fontSize: "2.5em",
-  paddingLeft: "1.5em",
-  marginBottom: ".5rem",
+  fontSize: "3em",
+  paddingLeft: ".5em",
   fontWeight: "500",
-  lineHeight: "1.2",
+  
 };
 const nav = {
   paddingRight: "4em",
@@ -28,13 +27,19 @@ const nav = {
   margin: "0",
   alignItems: "center",
 };
+const Quixote = {
+  width: '10%',
+  marginRight: '1rem',
+  marginBottom: '.5em',
+  marginTop: '.5em',
+}
 
 function NavBar({ currentPage, handlePageChange }) {
   return (
     <div style={style1}>
       <header className="blog-header ">
         <a style={logo} href="/">
-          GG
+          <img src="./img/quixote.png" style={Quixote} />GG
         </a>
       </header>
         <div className="col-4 d-flex justify-content-end align-items-center">
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,67 +1,147 @@
-import games from "../components/gamesList";
-import GamesCard from "../components/gamesCard";
-import Heading from "../components/heading";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Body({ currentPage, handlePageChange }) {
-  const Style={
-    marginTop: '10px',
-    height: '40px',
-  }
-  const ImgStyle={
-    height: '550px',
-    width: '700px',
+  const Style = {
+    marginTop: "10px",
+    height: "40px",
+  };
+  const ImgStyle = {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    height: "550px",
+    width: "700px",
+    borderRadius: '30px',
+  };
+
+  const BntStyle = {
+    height: '50px',
+    width: '100px',
+    marginTop: 'auto',
+    marginBottom: 'auto',
+    marginLeft: '10%',
+    marginRight: '10%',
+    borderRadius: '30px',
+  };
+
+  const BtmBtn = {
+    marginLeft: '10px',
+    borderRadius: '10px',
   }
 
   return (
-    <div >
-  
-    <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel" >
-  <div className="carousel-indicators" style={Style}>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      <Link to="/rock-paper-scissors" className="p-2 text-white">
-      <img src="./img/rps.png" class="d-block w-100" style={ImgStyle} alt="..."/>
-      </Link>
-      <div className="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
+    <div>
+      <div
+        id="carouselExampleCaptions"
+        className="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-indicators" style={Style}>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="0"
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+            style={BtmBtn}
+          >RPS</button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+            style={BtmBtn}
+          >TTT</button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+            style={BtmBtn}
+          >SNK</button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <div style={ImgStyle}>
+            <Link to="/rock-paper-scissors" className="p-2 text-white">
+              <img
+                src="./img/rps1.png"
+                class="d-block w-100 rounded"
+                alt="..."
+              />
+            </Link>
+            </div>
+            <div className="carousel-caption d-none d-md-block">
+              <h5>Rock | Paper | Scissors </h5>
+              <p>
+                How many times can you beat Quixote?
+              </p>
+            </div>
+          </div>
+          <div className="carousel-item">
+          <div style={ImgStyle}>
+            <Link to="/tic-tac-toe" className="p-2 text-white">
+              <img
+                src="./img/tictac.png"
+                className="d-block w-100"
+                alt="..."
+              />
+            </Link>
+            </div>
+            <div className="carousel-caption d-none d-md-block">
+              <h5>Tic-Tac-Toe</h5>
+              <p>
+                Good ol Xs and Os. PvP.
+              </p>
+            </div>
+          </div>
+          <div className="carousel-item">
+          <div style={ImgStyle}>
+            <Link to="/connect-four" className="p-2 text-white">
+              <img
+                src="./img/tictac.png"
+                className="d-block w-100"
+                alt="..."
+              />
+            </Link>
+            </div>
+            <div className="carousel-caption d-none d-md-block">
+              <h5>Snake Game</h5>
+              <p>
+                Eat the apples, don't touch the walls... Stay alive.
+              </p>
+            </div>
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="prev"
+          style={BntStyle}
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden"></span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="next"
+          style={BntStyle}
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden"></span>
+        </button>
       </div>
     </div>
-    <div className="carousel-item">
-    <Link to="/tic-tac-toe" className="p-2 text-white">
-      <img src="./img/tictac.png" className="d-block w-100" style={ImgStyle} alt="..." />
-      </Link>
-      <div className="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
-    </div>
-    <div className="carousel-item">
-    <Link to="/connect-four" className="p-2 text-white">
-      <img src="./img/tictac.png" className="d-block w-100" style={ImgStyle} alt="..." />
-      </Link>
-      <div className="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
-    </div>
-  </div>
-  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Previous</span>
-  </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
-  </button>
-</div>
-</div>
   );
 }
 
