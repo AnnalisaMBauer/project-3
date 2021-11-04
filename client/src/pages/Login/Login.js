c12d33d6cc7b3912b0175a5ed514450cbb8eca73
Andrew Haight <haight.haight@gmail.com>
2021-11-04 15:38:21 -0700

    login styled to match contact style

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -4,12 +4,18 @@ import LoginForm from "./LoginForm";
 import SignupForm from "./SignupForm";
 
 export default function Login() {
+  const Style ={
+    fontFamily: 'Orbitron',
+    marginBottom: 'auto',
+  }
   return (
-    <div>
+    <div className="container">
+    <div style={Style}>
       <h1>Login</h1>
       <LoginForm />
       <h1>Signup</h1>
       <SignupForm />
+    </div>
     </div>
   );
 }
