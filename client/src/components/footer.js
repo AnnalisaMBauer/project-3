9df829a63238a605a3517809181fb83255a662c8
Andrew Haight <haight.haight@gmail.com>
2021-11-07 15:54:54 -0700

    added home link on footer

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "../App.css"
+import { Link } from 'react-router-dom'
 
 function Footer() {
     const Style = {
@@ -9,6 +10,7 @@ function Footer() {
         <div className="fixed-bottom" style={Style}>
         <footer className="blog-footer mt-5">
             <div className="foot-box bg-dark text-center text-white">
+                <div className="pt-2"><Link to='/'>Back to Home</Link></div>
                 <div className="copyRight p-3">© 2021 Geek Games</div>
             </div>
       </footer>
